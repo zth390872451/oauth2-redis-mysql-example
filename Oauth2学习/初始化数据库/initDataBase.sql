3d3ef4fea47cba86d40cc92e7ce09d5947d62589
zth390872451 <zth390872451@gmail.com>
2019-01-02 20:44:46 +0800

    数据初始化文件更新

--- Oauth2学习/初始化数据库/initDataBase.sql
+++ Oauth2学习/初始化数据库/initDataBase.sql
@@ -10,10 +10,26 @@ Target Server Type    : MYSQL
 Target Server Version : 50639
 File Encoding         : 65001
 
-Date: 2019-01-01 17:59:12
+Date: 2019-01-02 20:44:22
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for member
+-- ----------------------------
+DROP TABLE IF EXISTS `member`;
+CREATE TABLE `member` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of member
+-- ----------------------------
+INSERT INTO `member` VALUES ('1', 'e10adc3949ba59abbe56e057f20f883e', 'username');
 
 -- ----------------------------
 -- Table structure for oauth_client_details
@@ -37,23 +53,5 @@ CREATE TABLE `oauth_client_details` (
 -- ----------------------------
 -- Records of oauth_client_details
 -- ----------------------------
-INSERT INTO `oauth_client_details` VALUES ('client_auth_mode', 'oauth2-client', '$2a$10$TiQGOXIhO2eWFMcxlGY2Vu1MrEFr30gKv84q7j/lkJyRiZgh6DieG', 'read,write', 'client_credentials,refresh_token', null, null, '2592000', null, null, null);
-INSERT INTO `oauth_client_details` VALUES ('password_auth_mode', 'oauth2-client',
-'$2a$10$TiQGOXIhO2eWFMcxlGY2Vu1MrEFr30gKv84q7j/lkJyRiZgh6DieG', 'read', 'refresh_token,password', null, 'ROLE_MEMBER',
-'2592000', null, null, null);
-
--- ----------------------------
--- Table structure for member
--- ----------------------------
-DROP TABLE IF EXISTS `member`;
-CREATE TABLE `member` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `password` varchar(255) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of member
--- ----------------------------
-INSERT INTO `member` VALUES ('1', 'e10adc3949ba59abbe56e057f20f883e', 'member_name');
+INSERT INTO `oauth_client_details` VALUES ('client_auth_mode', 'oauth2-resource', '$2a$10$TiQGOXIhO2eWFMcxlGY2Vu1MrEFr30gKv84q7j/lkJyRiZgh6DieG', 'read,write', 'client_credentials,refresh_token', null, 'ROLE_ADMIN', '2592000', null, null, null);
+INSERT INTO `oauth_client_details` VALUES ('password_auth_mode', 'oauth2-resource', '$2a$10$TiQGOXIhO2eWFMcxlGY2Vu1MrEFr30gKv84q7j/lkJyRiZgh6DieG', 'read', 'refresh_token,password', null, 'ROLE_MEMBER', '2592000', null, null, null);

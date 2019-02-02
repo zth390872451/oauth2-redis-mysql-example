b965e06acab4f11522c689af8bb760d2f55e78be
zth390872451 <zth390872451@gmail.com>
2019-02-02 17:04:32 +0800

    无用Bean配置

--- oauth2-resource/src/main/java/com/company/config/DataStoreConfig.java
+++ oauth2-resource/src/main/java/com/company/config/DataStoreConfig.java
@@ -2,8 +2,6 @@ package com.company.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -24,14 +22,5 @@ public class DataStoreConfig {
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
         converter.setSigningKey("123");
         return converter;
-    }
-
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        defaultTokenServices.setSupportRefreshToken(true);
-        return defaultTokenServices;
     }
 }
